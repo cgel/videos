44b254348458fa0962a609f81895ead50748c97c
Carles Gelada <cgel.saez@gmail.com>
2021-12-30 18:49:50 +0100

    refactor

--- scenes.py
+++ scenes.py
@@ -25,33 +25,7 @@ f_text = 'f(x,y) = x^2 - y^2 -3xy - x'
 # Potentially a Scene introducing how to read an elevation map
 
 # the simplest version
-class BruteForce(Scene):
-    def construct(self, low_q=True):
-        self.low_q = low_q
-        self.grid_lag = 0.01
-        self.x_range = [-6, 6, 2]
-        self.y_range = [-6, 6, 2]
-        print('top right', f(6,6))
-        print('top left', f(-6,6))
-        print('bottom right', f(6,-6))
-        print('bottom left', f(-6,-6))
-
-        # Full screen color map
-        self.draw_axes()
-        self.make_elevation_map()
-        self.play(FadeIn(self.elev_img))
-
-        # Display grid of grey dots
-        self.make_gird()
-        self.play(self.fade_in_grid_animation)
-        self.wait()
-        self.play(FadeOut(self.elev_img))
-        # Perform the measurement 1 by 1. The dots pick up the color.
-        self.play(self.color_points_by_elev_animation)
-        # Hightlight the loest dot in the screen
-        self.highlight_best_point()
-        self.wait(2)
-
+class SearchScene(Scene):
 
     def draw_axes(self):
         # TODO: figure out why length and unit_size aren't working. Make axes squared
@@ -64,7 +38,6 @@ class BruteForce(Scene):
                                         'length': 1}
                         )
         self.labels = self.axes.get_axis_labels(x_label="x", y_label="y")
-#        self.add(self.axes)
         self.play(Create(self.axes))
         self.play(FadeIn(self.labels))
 
@@ -108,17 +81,81 @@ class BruteForce(Scene):
         self.grid_values = [f(*coord) for coord in grid_coords]
         self.grid_colors = [color_map(val, np_mode=False) for val in self.grid_values]
 
-        fadein_points_animation = [FadeIn(point) for point in self.grid_points]
-        self.fade_in_grid_animation = AnimationGroup(*reversed(fadein_points_animation), lag_ratio = self.grid_lag)
+    def sample_random_points(self, n):
+        return np.random.randint(0, len(self.grid_points), [n])
 
-        color_points_by_elev_animation = [ point.animate.set_fill(color) for point, color in zip(self.grid_points, self.grid_colors)]
-        self.color_points_by_elev_animation = AnimationGroup(*reversed(color_points_by_elev_animation), lag_ratio = self.grid_lag)
+    def get_neighbors(self, i):
+        pass
 
-    def highlight_best_point(self):
-        fade_out_points_animation = [point.animate.set_opacity(0.3) for point in self.grid_points]
-        best_point = np.argmin(self.grid_values)
+    def best_point(self, point_indices=None):
+        if  point_indices is None: point_indices = list(range(len(self.grid_points)))
+        point_values = [self.grid_values[i] for i in point_indices]
+        best_point = np.argmin(point_values)
+        return best_point
+
+    def fadeout_points(self, point_indices=None):
+        if  point_indices is None: point_indices = list(range(len(self.grid_points)))
+        fadeout_points_animation = [FadeOut(self.grid_points[i]) for i in point_indices]
+        anim = AnimationGroup(*reversed(fadeout_points_animation), lag_ratio = self.grid_lag)
+        self.play(anim)
+
+
+    def fadein_points(self, point_indices=None, color_by_elevation=False):
+        if  point_indices is None: point_indices = list(range(len(self.grid_points)))
+        if color_by_elevation:
+            fadein_points_animation = [FadeIn(self.grid_points[i].set_fill(self.grid_colors[i])) for i in point_indices]
+        else:
+            fadein_points_animation = [FadeIn(point) for point in self.grid_points]
+        anim = AnimationGroup(*reversed(fadein_points_animation), lag_ratio = self.grid_lag)
+        self.play(anim)
+
+    def color_points(self, point_indices=None, color_by_elevation=False):
+        if  point_indices is None: point_indices = list(range(len(self.grid_points)))
+        if color_by_elevation:
+            fadein_points_animation = [self.grid_points[i].animate.set_fill(self.grid_colors[i]) for i in point_indices]
+        else:
+            fadein_points_animation = [self.grid_points[i].animate.set_fill(WHITE) for i in point_indices]
+        anim = AnimationGroup(*reversed(fadein_points_animation), lag_ratio = self.grid_lag)
+        self.play(anim)
+
+    def darken_points_except_best(self, point_indices=None):
+        if  point_indices is None: point_indices = list(range(len(self.grid_points)))
+        fade_out_points_animation = [self.grid_points[i].animate.set_opacity(0.3) for i in point_indices]
+        best_point = self.best_point(point_indices)
         del fade_out_points_animation[best_point]
         self.play(AnimationGroup(*fade_out_points_animation))
+
+
+class BruteForce(SearchScene):
+
+    def construct(self, low_q=True):
+        # TODO: automate the low_q from config
+        self.low_q = low_q
+        self.grid_lag = 0.01
+        self.x_range = [-6, 6, 2]
+        self.y_range = [-6, 6, 2]
+        print('top right', f(6,6))
+        print('top left', f(-6,6))
+        print('bottom right', f(6,-6))
+        print('bottom left', f(-6,-6))
+
+        # Full screen color map
+        self.draw_axes()
+        self.make_elevation_map()
+        self.play(FadeIn(self.elev_img))
+
+        # Display grid of grey dots
+        self.make_gird()
+        #self.play(self.fade_in_grid_animation)
+        self.fadein_points(color_by_elevation=False)
+        self.wait()
+        self.play(FadeOut(self.elev_img))
+        # Perform the measurement 1 by 1. The dots pick up the color.
+        self.color_points(color_by_elevation=True)
+        # Hightlight the loest dot in the screen
+        self.darken_points_except_best()
+        self.wait(2)
+        self.fadeout_points()
 
 
 class TheCurseOfDimensionality(ThreeDScene):
@@ -191,39 +228,98 @@ class TheCurseOfDimensionality(ThreeDScene):
         self.wait(3)
 
 
-class ExponentialGrouth(Scene):
+class ExponentialGrowth(Scene):
     def construct(self):
-        ax = Axes(
-            x_range=[0, 10], y_range=[0, 100, 10], axis_config={"include_tip": False}
-        )
+        axes = self.get_zoom_axes()
+
+    def get_zoom_axes(self):
+        axes = Axes(x_range=[0,30, 2],
+                    y_range=[0, 1000000, 100000])
+        self.add(axes)
+
+        print(list(axes.y_axis.ticks))
+
+        def get_y_labels(axes, y_values):
+            labels = VGroup()
+            for y in y_values:
+                try:
+                    tick = axes.y_axis.ticks[y]
+                    if y < 1000:
+                        label = Tex(f"{y}")
+                    elif y < 1000000:
+                        label = Tex(f"{y/1000}k")
+                    else:
+                        label = Tex(f"{y/1000000}m")
+                    always(label.next_to, tick)
+                    labels.add(label)
+                except IndexError:
+                    pass
+            return labels
+
+        main_labels = get_y_labels(axes, range(3, 30, 5))
+        axes.y_labels = main_labels
+        axes.small_y_labels = get_y_labels(axes, range(1,6))
+
+        tiny_ticks = VGroup()
+        tiny_labels = VGroup()
+        for y in range(200, 1000, 200):
+            tick = axes.y_axis.ticks[0].copy()
+            point = axes.c2p(0, y)
+            tick.move_to(point)
+            label = Integer(y)
+            label.set_height(0.25)
+            always(label.next_to, tick, LEFT, SMALL_BUFF)
+            tiny_labels.add(label)
+            tiny_ticks.add(tick)
+
+        axes.tiny_y_labels = tiny_labels
+        axes.tiny_ticks = tiny_ticks
+        axes.add(main_labels)
+        origin = axes.c2p(0, 0)
+        self.add(Dot(axes.c2p(10,10000)))
+        axes.y_axis.stretch(25, 1, about_point=origin)
+        self.wait()
+        self.add(
+                    axes.tiny_y_labels, axes.tiny_ticks,
+                )
+        self.play(
+                    axes.y_axis.animate.stretch(0.2, 1, about_point= origin),
+                    FadeOut(axes.tiny_y_labels),
+                    FadeOut(axes.tiny_ticks),
+                    run_time=2,
+                )
+        return axes
 
 
-        # Show the exponential curve of the number of evaluations we need for each dimension. Highlight when we exceed our budget.
-        # Modify the resolution, see how the curve changes. Use numbers from script
-        # Zoom out to see when we exceed the budget
 
-class LocalSearch(Scene):
-    def construct(self):
-        # Mostly empty grid but with a few evaluations
-        # Hightlight the best current point
-        # highlight each neighbor drawing an arow and meause (have the point pick up color)
-        # Hightlight the new best point
-        # Iterate the procedue. Slow a couple of times.  Then faster until convergence.
 
-        # Change the elevation map to a new equation with local minima. Show how it fails to find the best option.
+class LocalSearch(SearchScene):
 
-        # Sow a zig zag path, or a spiral path that ends up going through all the points in the grid.
+    def construct(self, low_q=True):
+        # TODO: automate the low_q from config
+        self.low_q = low_q
+        self.grid_lag = 0.01
+        self.x_range = [-6, 6, 2]
+        self.y_range = [-6, 6, 2]
+        print('top right', f(6,6))
+        print('top left', f(-6,6))
+        print('bottom right', f(6,-6))
+        print('bottom left', f(-6,-6))
 
-        # Still a problem.
-        still_a_problem = FadeIn('There is still a problem')
-        self.play(still_a_problem)
-        self.wait(2)
-        self.play(FadeOut(still_a_problem))
+        # Full screen color map
+        self.draw_axes()
+        self.make_elevation_map()
 
-        # Show number of neighbors in 1D = 2. A point with 2 arrows
-        # Show number of neighbors in 2D = 4. A point with 4 arrows
-        # Show number of neighbors in 3D = 6. A point with 6 arrows
-        # Show general formula
+        # Display grid of grey dots
+        self.make_gird()
+        # Perform the measurement 1 by 1. The dots pick up the color.
+        random_point_indices = self.sample_random_points(5)
+        self.draw_points_by_elevation(random_point_indices)
+        self.wait(1)
+        self.fade_points_except_best(random_point_indices)
+
+
+
 
 class TheGradient(Scene):
     def construct(self):
